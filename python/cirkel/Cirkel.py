82f091fe5a9470093645ddef33102e699808adf7
Viktor Hoffgaard Kallenberg <viktor@seven.kallenberg.dk>
2021-01-11 18:43:28 +0100

    cirkel tilføjet

---
+++ python/cirkel/Cirkel.py
@@ -0,0 +1,75 @@
+Python 3.5.3 (default, Nov 18 2020, 21:09:16) 
+[GCC 6.3.0 20170516] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> from turtle import *
+>>> pendown()
+>>> for n in range(24)
+SyntaxError: invalid syntax
+>>> for n in range(24):
+	forward(10)
+	right(15)
+
+	
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
+
+>>> 
+>>> 
